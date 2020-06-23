fc783d73ee4831ad36170e811fed486fb73f1caf
maitasawilia <maitasaw@gmail.com>
2020-06-23 22:24:31 +0700

    Tugas 2 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas2.js
@@ -0,0 +1,17 @@
+var number = 25;
+var string = "huruf";
+var boolean = false;
+var cm = 180;
+if(cm>170)
+tinggi = true;
+else
+tinggi = false;
+
+var nul = "nihil";
+var array = [9,8,7,6];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(nul);
+console.log(array);
